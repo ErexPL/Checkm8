e69b235aed87df86b2fa860e23804d41a49360cb
ErexPL <zys.dawid@gmail.com>
2024-05-06 22:10:05 +0200

    More mobile adjustments

--- main.js
+++ main.js
@@ -147,69 +147,73 @@ function linkScroll(newScrollPosition) {
     returnImg.classList.add('slide-down');
 }
 
-function startRandomNumbers() {
-    let finalText = 'Checkm8'.split('');
-    let displayText = [];
-    let startTime = new Date().getTime();
-
-    finalText.forEach((_, index) => {
-        const stopAfter = 500 + index * 500;
-
-        let intervalId = setInterval(() => {
-            if (new Date().getTime() - startTime >= stopAfter) {
-                clearInterval(intervalId);
-                displayText[index] = finalText[index];
-                h1.textContent = displayText.join('');
-            } else {
-                displayText[index] = Math.floor(Math.random() * 10).toString();
-                h1.textContent = displayText.join('');
-            }
-        }, 100);
-    });
-}
-
-function startTextCycle() {
-    let states = ['The Ultimate AI Chatbot', 'Setting New Standards', 'Just Straight Up Better'];
-    let currentState = 0;
-
-    function typeText(text, callback) {
-        let index = 0;
-        let intervalId = setInterval(() => {
-            if (index < text.length) {
-                p.textContent += text[index];
-                index++;
-            } else {
-                clearInterval(intervalId);
-                setTimeout(() => callback(), 2000);
-            }
-        }, 100);
-    }
-
-    function backspaceText(callback) {
-        let text = p.textContent;
-        let intervalId = setInterval(() => {
-            if (text.length > 0) {
-                text = text.substring(0, text.length - 1);
-                p.textContent = text;
-            } else {
-                clearInterval(intervalId);
-                callback();
-            }
-        }, 100);
-    }
-
-    function setNextState() {
-        let text = states[currentState];
-        typeText(text, () => {
-            backspaceText(() => {
-                currentState = (currentState + 1) % states.length;
-                setNextState();
-            });
+if (window.innerWidth > 750) {
+    function startRandomNumbers() {
+        let finalText = 'Checkm8'.split('');
+        let displayText = [];
+        let startTime = new Date().getTime();
+    
+        finalText.forEach((_, index) => {
+            const stopAfter = 500 + index * 500;
+    
+            let intervalId = setInterval(() => {
+                if (new Date().getTime() - startTime >= stopAfter) {
+                    clearInterval(intervalId);
+                    displayText[index] = finalText[index];
+                    h1.textContent = displayText.join('');
+                } else {
+                    displayText[index] = Math.floor(Math.random() * 10).toString();
+                    h1.textContent = displayText.join('');
+                }
+            }, 100);
         });
     }
-
-    setNextState();
+    
+    function startTextCycle() {
+        let states = ['The Ultimate AI Chatbot', 'Setting New Standards', 'Just Straight Up Better'];
+        let currentState = 0;
+    
+        function typeText(text, callback) {
+            let index = 0;
+            let intervalId = setInterval(() => {
+                if (index < text.length) {
+                    p.textContent += text[index];
+                    index++;
+                } else {
+                    clearInterval(intervalId);
+                    setTimeout(() => callback(), 2000);
+                }
+            }, 100);
+        }
+    
+        function backspaceText(callback) {
+            let text = p.textContent;
+            let intervalId = setInterval(() => {
+                if (text.length > 0) {
+                    text = text.substring(0, text.length - 1);
+                    p.textContent = text;
+                } else {
+                    clearInterval(intervalId);
+                    callback();
+                }
+            }, 100);
+        }
+    
+        function setNextState() {
+            let text = states[currentState];
+            typeText(text, () => {
+                backspaceText(() => {
+                    currentState = (currentState + 1) % states.length;
+                    setNextState();
+                });
+            });
+        }
+    
+        setNextState();
+    }
+    
+    startRandomNumbers();
+    startTextCycle();
+} else {
+    p.textContent = 'The Ultimate AI Chatbot';
 }
-
-startRandomNumbers();
-startTextCycle();
